cad027ced5a246944de5da19696cee576caca553
David Mathews <david.mathews@mines.sdsmt.edu>
2024-02-07 12:16:00 -0700

    Extract simulation testing in main to separate function.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+void TestSim(string filename);
+
 int main(int argc, char** argv)
 {
 	// Initialize variables
@@ -13,13 +15,26 @@ int main(int argc, char** argv)
 
 	// Data Arguments
 	// Load Simulation from File
-	Simulation sim = Simulation();
-	sim.initializeSim("TestSim.sim");
-	sim.printSim();
-	sim.printSimFancy();
-	sim.printSimPDF("TestSim"); //Currenly doesn't handle .pdf extensions
+	TestSim("TestSim.sim");
 	// Set variables
 	// Epochs Loop (Loop once for each Episode)
 		// Episode Loop (Loop over time-steps t)
   return 0;
+}
+
+void TestSim(string filename)
+{
+	string outFilename;
+	Simulation sim = Simulation();
+	sim.initializeSim(filename);
+	sim.printSim();
+	sim.printSimFancy();
+	// Took this from printSimPDF.cpp. by Dr. Larry Pyeatt.
+	// Should remove .sim extension for output file name purposes.
+  if((filename.find_last_of('.') != string::npos) &&
+     (filename.substr(filename.find_last_of('.'),4) == ".sim"))
+    outFilename=filename.substr(0,filename.find_last_of('.'));
+	else
+		outFilename = filename;
+	sim.printSimPDF(outFilename); 
 }
